1764279e709776aadebd2d85f0fe89fece89ffd0
Veikko Pankakoski <veikko@rautanenyhtiot.fi>
2025-06-06 11:05:22 +0300

    Bug fix: Only draw.io type templates are rendered in UI

--- netbox_ninja_plugin/templatetags/ninja_tags.py
+++ netbox_ninja_plugin/templatetags/ninja_tags.py
@@ -16,3 +16,13 @@ def app_label(obj):
 @register.filter
 def model_name(obj):
     return getattr(obj._meta, "model_name", "")
+
+
+@register.filter
+def any_drawio(templates):
+    return any(t.output_type == "drawio" for t in templates)
+
+
+@register.filter
+def as_list(obj):
+    return [obj]

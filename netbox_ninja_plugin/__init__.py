5a40221fdc6cbc72a4c0a1dc6bdc567fcf3f309a
Veikko Pankakoski <45948003+hirvi0@users.noreply.github.com>
2025-09-03 14:22:49 +0300

    Support Netbox v4.3.7 (#24)
    * Deleted `develop/base_requirements.txt` and now using Netbox's up-to-date `requirements.txt` in local development environment.
    
    Co-authored-by: Veikko Pankakoski <veikko@rautanenyhtiot.fi>

--- netbox_ninja_plugin/__init__.py
+++ netbox_ninja_plugin/__init__.py
@@ -17,7 +17,7 @@ class NinjaPluginConfig(PluginConfig):
     description = "Dynamic configuration files and images from NetBox."
     version = __version__
     min_version = "4.2.3"
-    max_version = "4.3.6"
+    max_version = "4.3.7"
     author = "rautanen.io"
     author_email = "veikko@rautanenyhtiot.fi"
     required_settings = []
